6690e080c76246c61f8f425cd0e5ab6d07283f68
Hanga Dormán <dormanhanga@gmail.com>
2023-02-15 14:57:50 +0100

    tidy up

--- graph.py
+++ graph.py
@@ -1,16 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-from graphviz import Digraph
 import networkx as nx
-from itertools import combinations
+from graphviz import Digraph
 
 
-"""
----------------------
-Define causal effects
----------------------
-"""
+# define causal effects
 
 edges = (
     (
@@ -44,11 +36,7 @@ edges = (
 )
 
 
-"""
----------------------
-Isolate relevant path
----------------------
-"""
+# isolate relevant path
 
 path_nodes = {
     "gender",
@@ -59,25 +47,28 @@ path_nodes = {
     "advanced skills",
 }
 
+
 def subgraph_edges(all_edges, selected_nodes, exclude=set()):
-    
+
     graph = nx.DiGraph()
     graph.add_edges_from(all_edges)
     subgraph = nx.subgraph(graph, selected_nodes)
-    
+
     return set(subgraph.edges()) - exclude
 
 
-path_edges = subgraph_edges(edges, path_nodes, exclude={("gender", "motivation"),
-         ("gender", "exposure"),
-         ("early stimulation", "motivation")})
+path_edges = subgraph_edges(
+    edges,
+    path_nodes,
+    exclude={
+        ("gender", "motivation"),
+        ("gender", "exposure"),
+        ("early stimulation", "motivation"),
+    },
+)
 
 
-"""
-------------------------
-Construct causal diagram
-------------------------
-"""
+# construct causal diagram
 
 g = Digraph()
 attr = {"color": "orange"}
@@ -90,33 +81,33 @@ for edge in path_edges:
 
 for edge in set(edges) - path_edges:
     g.edge(*edge)
-    
-    
-"""
----------------------
-Construct subgraph 1.
----------------------
-"""
-    
-path_1 = {"family", "genetics", "early interests", "gender", "early stimulation", "early skills"}
+
+
+# construct subgraph 1.
+
+path_1 = {
+    "family",
+    "genetics",
+    "early interests",
+    "gender",
+    "early stimulation",
+    "early skills",
+}
 g_1_edges = subgraph_edges(edges, path_1)
 g_1 = Digraph()
 
 for node in path_1 & path_nodes:
     g_1.node(node, _attributes=attr)
-    
+
 for edge in g_1_edges & path_edges:
     g_1.edge(*edge, _attributes=attr)
-    
+
 for edge in g_1_edges - path_edges:
     g_1.edge(*edge)
-    
 
-"""
----------------------
-Construct subgraph 2.
----------------------
-"""
+
+# construct subgraph 2.
+
 
 path_2 = {"early skills", "motivation", "exposure", "advanced skills"}
 g_2_edges = subgraph_edges(edges, path_2)
@@ -124,9 +115,9 @@ g_2 = Digraph()
 
 for node in path_2:
     g_2.node(node, _attributes=attr)
-    
+
 for edge in g_2_edges:
     g_2.edge(*edge, _attributes=attr)
-    
+
 for edge in set(edges) - g_2_edges:
     g_2.edge(*edge)
